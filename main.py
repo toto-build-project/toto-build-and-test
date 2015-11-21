fe321d862ac341e55d6aa1ab372a38e7bcd5e8e5
Casey McGinley <cmcg513@gmail.com>
2015-11-21 13:19:13 -0500

    corrected indentation to 2 spaces

--- main.py
+++ main.py
@@ -8,73 +8,72 @@ import shutil
 import hashlib
 
 def main():
-	command = sys.argv[1]
-	stdin = sys.argv[2]
-	details = sys.argv[3]
+  command = sys.argv[1]
+  stdin = sys.argv[2]
+  details = sys.argv[3]
 
-	set_stdin = True
-	if stdin.strip() == '-':
-		set_stdin = False
+  set_stdin = True
+  if stdin.strip() == '-':
+    set_stdin = False
 
-	metadata = dict()
-	metadata['variables'] = dict()
-	metadata['application'] = dict()
+  metadata = dict()
+  metadata['variables'] = dict()
+  metadata['application'] = dict()
 
-	process_env_vars(metadata)
-	stdout, stderr = exec_cmd(command, set_stdin, stdin)
-	process_app_data(metadata, command, set_stdin, stdin, stdout, stderr, details)
-	genJSON(metadata, "metadata")
+  process_env_vars(metadata)
+  stdout, stderr = exec_cmd(command, set_stdin, stdin)
+  process_app_data(metadata, command, set_stdin, stdin, stdout, stderr, details)
+  genJSON(metadata, "metadata")
 
 def exec_cmd(command, set_stdin, stdin):
-	if set_stdin:
-		input_path = os.path.join(os.getcwd(),stdin)
-		f = open(input_path,"r")
-		proc = subprocess.Popen(command, stdin=f, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-		f.close()
-	else:
-		proc = ubprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	return proc.communicate()
+  if set_stdin:
+    input_path = os.path.join(os.getcwd(),stdin)
+    f = open(input_path,"r")
+    proc = subprocess.Popen(command, stdin=f, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    f.close()
+  else:
+    proc = ubprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+  return proc.communicate()
 
 def process_env_vars(metadata):
-	uname = os.uname()
+  uname = os.uname()
 
-	metadata['variables']['os'] = dict()
-	metadata['variables']['os']['kernel'] = uname[0]
-	metadata['variables']['os']['release'] = uname[2]
-	metadata['variables']['os']['version'] = uname[3]
+  metadata['variables']['os'] = dict()
+  metadata['variables']['os']['kernel'] = uname[0]
+  metadata['variables']['os']['release'] = uname[2]
+  metadata['variables']['os']['version'] = uname[3]
 
-	metadata['variables']['hostname'] = uname[1]
-	metadata['variables']['cpu_arch'] = uname[4]
-	metadata['variables']['timestamp'] = str(datetime.datetime.utcnow())
-	metadata['variables']['user'] = getpass.getuser()
-	metadata['variables']['curr_working_dir'] = os.getcwd()
+  metadata['variables']['hostname'] = uname[1]
+  metadata['variables']['cpu_arch'] = uname[4]
+  metadata['variables']['timestamp'] = str(datetime.datetime.utcnow())
+  metadata['variables']['user'] = getpass.getuser()
+  metadata['variables']['curr_working_dir'] = os.getcwd()
 
 def process_app_data(metadata, command, set_stdin, stdin, stdout, stderr, details):
-	metadata['application']['command'] = command
+  metadata['application']['command'] = command
 
-	cwd = os.getcwd()
+  cwd = os.getcwd()
 
-	if set_stdin:
-		input_path = os.path.join(cwd,"in")
-		shutil.copyfile(stdin,input_path)
-		metadata['application']['input_hash'] = get_hash(input_path)
-		metadata['application']['input_path'] = input_path
-	else:
-		metadata['application']['input_hash'] = None
-		metadata['application']['input_path'] = None
+  if set_stdin:
+    input_path = os.path.join(cwd,"in")
+    shutil.copyfile(stdin,input_path)
+    metadata['application']['input_hash'] = get_hash(input_path)
+    metadata['application']['input_path'] = input_path
+  else:
+    metadata['application']['input_hash'] = None
+    metadata['application']['input_path'] = None
 
-	output_path = os.path.join(cwd,"out")
-	write_to_file(stdout, output_path)
-	metadata['application']['output_hash'] = get_hash(output_path)
-	metadata['application']['output_path'] = output_path
+  output_path = os.path.join(cwd,"out")
+  write_to_file(stdout, output_path)
+  metadata['application']['output_hash'] = get_hash(output_path)
+  metadata['application']['output_path'] = output_path
 
-	err_path = os.path.join(cwd,"err")
-	write_to_file(stderr, err_path)
-	metadata['application']['err_hash'] = get_hash(err_path)
-	metadata['application']['err_path'] = err_path
+  err_path = os.path.join(cwd,"err")
+  write_to_file(stderr, err_path)
+  metadata['application']['err_hash'] = get_hash(err_path)
+  metadata['application']['err_path'] = err_path
 
-	metadata['application']['details'] = details
-
+  metadata['application']['details'] = details
 
 main()
 

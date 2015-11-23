c9a5f402d63c5283f9344c0576fa18c325039e44
Wilson <wilsonli@nyu.edu>
2015-11-22 23:00:20 -0500

    created signing module

---
+++ signing.py
@@ -0,0 +1,39 @@
+"""
+<Program Name>
+  signing.py
+<Author>
+  Casey McGinley
+  Fernando Maymi
+  Catherine Eng
+  Justin Valcarel
+  Wilson Li
+<Started>
+  November 22, 2015
+<Copyright>
+  See LICENSE for licensing information.
+<Purpose>
+  To sign the metadata.
+"""
+
+import tuf.keys
+import tuf.sig
+import tuf.util
+
+def sign_json(data):
+  #signed - keys
+  rsakey_dict = tuf.keys.generate_rsa_key()
+	
+  #signatures - keyid, method, sig
+  signature = tuf.keys.create_signature(rsakey_dict, data)
+	
+  #signatures - keyid, method, sig
+  signature_dict = tuf.sig.generate_rsa_signature(signature, rsakey_dict)
+	
+  rsakey_dict["keyval"]["private"] = ""
+	
+  #what is difference between signature vs signature_dict?
+  data["signatures"] = signature
+  data["signed"] = rsakey_dict
+	
+  return data
+

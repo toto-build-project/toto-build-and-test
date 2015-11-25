d8a1880849133004b24d8a51bf60895bbf60ae8b
Wilson <wilsonli@nyu.edu>
2015-11-25 15:07:14 -0500

    changed signing and verifying to use canonical json

--- signing.py
+++ signing.py
@@ -16,12 +16,13 @@
   See LICENSE for licensing information.
 
 <Purpose>
-  To sign the metadata.
+  To sign and verify the metadata.
 """
 
 import tuf.keys
 import tuf.sig
 import tuf.util
+
 
 def print_object(obj_desc, object):
   # Return on prints, potentially remove this function
@@ -34,15 +35,6 @@ def print_object(obj_desc, object):
   print "\n==============================\n" 
 
 
-def test_verify(orig, data):
-  signature = tuf.sig.generate_rsa_signature(rsa_key, data)
-
-  print "test_verify GOOD results:  " 
-  print tuf.keys.verify_signature(rsa_key, signature, data)
-  print "\ntest_verify BAD results:  " 
-  data =  {'AAAName': 'Zara', 'Age': 7, 'Class': 'First'}
-  print tuf.keys.verify_signature(rsa_key, signature, orig)
-  return
 
 
 
@@ -51,32 +43,39 @@ def sign_json(in_data):
   data = in_data.copy()
   print_object("INITIAL_DATA", data);
 
-  # generate rsa key
+  # generate rsa keys
   rsakey_dict = tuf.keys.generate_rsa_key()
   print_object("RSAKEY_DICT", rsakey_dict);
 
   # generate signature
-  signature = tuf.keys.create_signature(rsakey_dict, data)
-  print_object("SIGNATURE", signature);
+  #signature = tuf.keys.create_signature(rsakey_dict, data)
+  #print_object("SIGNATURE", signature);
+  rsa_signature  = tuf.sig.generate_rsa_signature(data, rsakey_dict)
+  print_object("RSA_SIGNATURE", rsa_signature);
 
-  rsa_signature  = tuf.sig.generate_rsa_signature(signature, rsakey_dict)
-  ##print "\nprivate=" + rsakey_dict["keyval"]["private"] + "]]"; 
-  ##print_object("RSA_SIGNATURE", rsa_signature);
-
-  # update data with key and signature
-  rsakey_dict["keyval"]["private"] = ""; 
-  data["signed"] = rsakey_dict
-  data["signatures"] = signature
+  # update metadata with public key and signature
+  rsakey_dict['keyval']['private'] = ""; 
+  data['signed'] = rsakey_dict
+  data['signatures'] = rsa_signature
   print_object("FINAL_DATA", data);
 
   return data
 
 
 
-def verify_json(data, retdata):
-  verify_state = tuf.keys.verify_signature(retdata["signed"], retdata["signatures"], data)
+
+
+def verify_json(data):
+  canonicalData = data.copy()
+  del canonicalData['signed']
+  del canonicalData['signatures']
+  canonicalData = tuf.formats.encode_canonical(canonicalData)
+  verify_state = tuf.keys.verify_signature(data['signed'], data['signatures'], canonicalData)
 
   return verify_state
+
+
+
 
 
 def run_test1():
@@ -86,12 +85,10 @@ def run_test1():
 
   # GOOD DATA - testing verify_json
   print "Good Test:  "
-  print verify_json(data, retdata)
+  print verify_json(retdata)
 
   # BAD DATA - testing verify_json
   print "Bad Test:  "
-  xdata =  {'BBBBBName': 'Zara', 'Age': 7, 'Class': 'First'}
-  print verify_json(xdata, retdata)
-
-
-
+  xdata = retdata
+  xdata['Name'] = 'FakeName'
+  print verify_json(xdata)

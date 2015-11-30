5b9c28ff8fd087cd6e4fd54be253125b2edc80de
ceinfo <korecat@gmail.com>
2015-11-29 21:10:00 -0500

    remove unnecessary comments

--- examples/c_code_proj/main.cpp
+++ examples/c_code_proj/main.cpp
@@ -26,9 +26,7 @@ int main(int argc, char *argv[])
     secret[0] = SECRET1; secret[1] = SECRET2;
     
 
-//while (1) {
-   // printf("The variable tat address is 0x%8x|%d (on stack)\n", &tat), &tat;
-
+  while (1) {
     printf("The variable secret's address is 0x%8x|%d (on stack)\n", &secret), &secret;
     printf("The variable secret's value is 0x%8x|%d (on heap)\n", secret, secret);
     printf("secret[0]'s address is 0x%8x|%d  (on heap)\n", &secret[0], &secret[0]);
@@ -36,10 +34,9 @@ int main(int argc, char *argv[])
     printf("The user_input address is 0x%8x|%d  (on stack) \n", &user_input, &user_input);
     printf("The int_input address is 0x%8x|%d (on stack)\n", &int_input, &int_input);
 
-/**
     printf("Please enter a decimal integer\n");
     scanf("%d", &int_input);  
-**/
+
     printf("Please enter a string\n");
     scanf("%s", user_input); /* getting a string from user */
     
@@ -51,6 +48,6 @@ int main(int argc, char *argv[])
     printf("The original secrets: 0x%x -- 0x%x\n", SECRET1, SECRET2);
     printf("The new secrets:      0x%x -- 0x%x\n", secret[0], secret[1]);
 
-//}
-    return 0;
+  }
+  return 0;
 }

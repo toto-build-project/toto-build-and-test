17b949100fe65d0199dc03912d1130b1149634e7
justinvalcarcel <justinvalcarcel@gmail.com>
2015-11-30 22:51:20 -0500

    Initial commit of setup.py Added more metadata and external library requirements

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@
 
 from setuptools import setup
 from setuptools import find_packages
+from codecs import open
 
 with open('README.md') as file_object:
   long_description = file_object.read()
@@ -51,20 +52,10 @@ setup(
     'Intended Audience :: Developers',
     'Natural Language :: English',
     'Topic :: Software Development :: Testing',
-    'Programming Lanugae :: Python :: 2',
+    'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7'
   ],
   keywords='testing verification framework',
   packages = find_packages(exclude=['tests', 'excludes/c_code_proj']),
-  install_requires=[
-    'tuf',
-    'hashlib',
-    'pycrypto',
-    'canonicaljson'
-    'iso8601',
-    'six',
-    'pycrypto==2.6.1',
-    'pynacl==0.2.3',
-    'cryptography==1.0'
-  ]
+  install_requires=['tuf', 'canonicaljson']
   )
